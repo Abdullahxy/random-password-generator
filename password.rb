8170793e9bea11bb7b5e7f346037837b34c6cf42
Abdullah <abdullah.shahid@devsinc.com>
2022-07-24 17:05:00 +0500

    Added comments.

--- password.rb
+++ password.rb
@@ -3,16 +3,22 @@
 # Takes input the length (fixnum < 20) and options (hash) from user
 # hash options: alphabets/numeric/special-characters
 # Returns a randomly generated password
-
 def generate_password(len = 10, options = { alphabets: true })
+  # Sanity checks to ensure password length is okay
   if len > 20 || len.negative?
     len = 20
   end
 
-  arr = get_arr(len,options)
+  # get_arr returns a skeleton array of length len with
+  # positions marked for each option
+  arr = get_arr(len, options)
+
+  # array to store passwords
   password = []
+
   selected = 0
 
+  # Iterating over array and randomly filling each option
   arr.each do |i|
     case i
     when 0
@@ -24,33 +30,56 @@ def generate_password(len = 10, options = { alphabets: true })
     end
     password << selected.chr
   end
+
+  # returning password as string
   password.join('')
 end
 
+# a method to randomly allocate positions to
+# options specified by the user
 def get_arr(len, options = {})
+  # seeding random function with current time
   srand(Time.now.to_i)
-  arr = []
+
+  # array to return
+  arr_to_return = []
+
+  # allocating positions to options
   len.times do
-    r = rand(3)
-    if r.eql? 0
-      arr << r
+
+    # getting a random number between 0 and 2
+    # 0 => alphabets
+    # 1 => numeric
+    # 2 => special
+    option = rand(3)
+
+    # default option is alphabets
+    if option.eql? 0
+      arr_to_return << option
       next
-    elsif r == 1 && options[:numeric]
-      arr << r
+    elsif option == 1 && options[:numeric]
+      arr_to_return << option
       next
-    elsif r == 1 && !(options[:numeric])
-      arr << 0
+    elsif option == 1 && !(options[:numeric])
+      arr_to_return << 0
       next
-    elsif r == 2 && options[:special]
-      arr << r
+    elsif option == 2 && options[:special]
+      arr_to_return << option
       next
     else
-      arr << 0
+      arr_to_return << 0
     end
   end
-  arr
+  arr_to_return
 end
 
+# Set password length from here
 password_length = 15
+
+# Takes password length and options as input and
+# Returns randomly generated password as per user needs
+# Options: alphabets, numeric, special
 pass = generate_password(password_length, numeric: true, special: true)
+
+# Prints password
 puts pass
